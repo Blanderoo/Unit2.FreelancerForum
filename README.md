e9e03f9b110969dc052469da6b005c2df3893dd3
blandero <landerosb08@gmail.com>
2023-10-19 18:21:35 -0600

    basic array defined

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # block19.Unit2.FreelancerForum
+
 https://fullstack.instructure.com/courses/731/assignments/30342?module_item_id=226970
---
+++ freelanceForum.js
@@ -0,0 +1,5 @@
+const freelancers =[
+{ name: "Alice", occupation: "Writer", rate: 30},
+{ name: "Bob", occupation: "Teacher", rate: 50}
+{ name: "Carol", occupation: "Programmer", rate: 70}
+];
